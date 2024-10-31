22a5a559d88076d99526518b80071bb07e4efb21
δυς <dys@dhappy.org>
2024-10-31 10:22:59 -0400

    moving beep to a file 🧀

--- src/components/TonesCircle.svelte
+++ src/components/TonesCircle.svelte
@@ -1,27 +1,7 @@
 <script>
-  import { A432 } from '../lib/notes'
+  import { beep } from '$lib/beep'
+  import { A432 } from '$lib/notes'
 
-  const beep = () => {
-    const ctx = new AudioContext()
-    const osc = ctx.createOscillator()
-    const gain = ctx.createGain()
-    const now = ctx.currentTime
-
-    const ramp = 0.01
-    const dur = 0.1
-
-    osc.frequency.value = A432[5].C
-
-    osc.connect(gain)
-    gain.connect(ctx.destination)
-
-    gain.gain.setValueAtTime(0, now)
-    gain.gain.exponentialRampToValueAtTime(1, now + ramp)
-    gain.gain.setValueAtTime(1, now + dur - ramp)
-    gain.gain.exponentialRampToValueAtTime(0.0001, now + dur)
-
-    osc.start(now)
-  }
 </script>
 
-<button on:click={beep}>Beep</button>
+<button on:click={() => beep(A432[5].C)}>Beep</button>
---
+++ src/lib/beep.ts
@@ -0,0 +1,21 @@
+export const beep = (
+  freq: number,
+  { ramp = 0.01, dur = 0.1 },
+) => {
+  const ctx = new AudioContext()
+  const osc = ctx.createOscillator()
+  const gain = ctx.createGain()
+  const now = ctx.currentTime
+
+  osc.frequency.value = freq
+
+  osc.connect(gain)
+  gain.connect(ctx.destination)
+
+  gain.gain.setValueAtTime(0, now)
+  gain.gain.exponentialRampToValueAtTime(1, now + ramp)
+  gain.gain.setValueAtTime(1, now + dur - ramp)
+  gain.gain.exponentialRampToValueAtTime(0.0001, now + dur)
+
+  osc.start(now)
+}
