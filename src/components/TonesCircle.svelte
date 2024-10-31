3f4ae817ff5ba07358d5742058c613b848d12679
δυς <dys@dhappy.org>
2024-10-31 15:13:51 -0400

    note selection \& sounding working ☪

--- src/components/TonesCircle.svelte
+++ src/components/TonesCircle.svelte
@@ -1,7 +1,9 @@
-<script>
+<script lang="ts">
   import clsx from 'clsx'
   import { beep } from '$lib/beep'
   import { A432 } from '$lib/notes'
+
+  type Indices = { i: number, j: number }
 
   let segCount = $state(12)
   let space = $state(1 / 5)
@@ -12,19 +14,61 @@
     A432.map((octave) => Object.values(octave)).flat()
   )
   let step = $state(0)
+  let selected = $state<Array<Indices>>([])
   let delay = $state(500)
+
   setInterval(() => {
     step = (step + 1) % segCount
+    const noteIdxs = (
+      selected
+      .filter(({ j }) => j === step)
+      .map(({ i }) => i)
+    )
+    noteIdxs.forEach((idx: number) => {
+      beep(notes[idx])
+    })
   }, delay)
+
+  const id = (i: number, j: number) => (
+    i + segCount + j
+  )
+
+  const toggle = (idxs: Indices) => {
+    const match = ({ i: ii, j: jj }) => (
+      ii === idxs.i && jj === idxs.j
+    )
+    const found = !!selected.find(match)
+    selected = (
+      found
+      ? selected.filter((idxs) => !match(idxs))
+      : [...selected, idxs]
+    )
+    return !found
+  }
+
+  const select = (elem: SVGElement) => {
+    elem.addEventListener('click', () => {
+      const present = toggle(
+        {
+          i: Number(elem.dataset.i),
+          j: Number(elem.dataset.j),
+        }
+      )
+      if(present) {
+        elem.classList.add('selected')
+      } else {
+        elem.classList.remove('selected')
+      }
+    })
+  }
 </script>
 
 <svg viewBox="0 0 500 500">
   <g
     transform={`rotate(
     ${
-      (360 / segCount) / 2
-      + (360 / segCount) * space / 2
-      + (360 / segCount) * -(segCount / 3)
+      (360 / segCount) / 2 + (360 / segCount) * space / 2
+      - (360 / segCount) * (segCount / 3)
     }
     250
     250
@@ -35,10 +79,19 @@
           cx="50%" cy="50%"
           r={`${(maxW * ((i + 1) / notes.length) + (maxW * (centerR / 100)))}%`}
           stroke-dasharray={`${width - 1} ${(segCount * width) - (width - 1)}`}
-          stroke-dashoffset={j * width}
+          stroke-dashoffset={(segCount - j) * width}
           pathLength={segCount * width}
-          class={clsx(step === segCount - j && 'active')}
-          on:click={() => beep(notes[i])}
+          data-i={i} data-j={j}
+          class={clsx(
+            `arcs-${j}`, `arc-${id(i, j)}`,
+            step === j && 'active',
+            !!selected.find(
+              ({ i: ii, j: jj }) => (
+                ii === i && jj === j
+              )
+            ) && 'selected',
+          )}
+          use:select
         />
       {/each}
     {/each}
@@ -54,7 +107,7 @@
     width: 100vw;
     height: 100vh;
     stroke: green;
-    stroke-width: 0.25vmin;
+    stroke-width: 0.5vmin;
     fill: none;
   }
   circle:hover {
@@ -64,11 +117,22 @@
   .active {
     stroke: orangered;
   }
+  .arcs-0 {
+    stroke-opacity: 0.5;
+  }
+  .arcs-5.active {
+    stroke: purple;
+  }
   circle.active:hover {
     stroke: cyan;
   }
   line {
     stroke: red;
-    stroke-width: 0.5vmin;
+    stroke-width: 1.5vmin;
+    stroke-linecap: round;
+  }
+  .selected {
+    stroke: yellow;
+    stroke-width: 3vmin;
   }
 </style>
