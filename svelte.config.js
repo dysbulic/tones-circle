cc6ee52cda6cd826e4d1378b0d74f05f48e56e5c
δυς <dys@dhappy.org>
2024-10-31 21:30:42 -0400

    building static 𣑊

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,9 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+// import adapter from '@sveltejs/adapter-auto'
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
+export const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
@@ -11,8 +12,11 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
-};
+		adapter: adapter({
+			fallback: '404.html',
+			strict: true,
+		}),
+	},
+}
 
-export default config;
+export default config
